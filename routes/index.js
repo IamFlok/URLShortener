2a32e494d4852ee2f07d74931e015e1a772969b1
Flok <xflokx@gmail.com>
2017-09-09 20:03:55 +0200

    working url to short function
    - takes original url parameter, creates a valid shorturl and displays
    - checks if original url is valid

--- routes/index.js
+++ routes/index.js
@@ -1,46 +1,106 @@
 var express = require('express');
-const mongoose = require('mongoose');
-const shortUrl = require('../models/shortUrl');
+var mongo = require('mongodb').MongoClient;
 var router = express.Router();
-
-//connect to database
-mongoose.connect(process.env.MONGODB_URI ||
-  'mongodb://fccuser:fCcUsER61@ds131384.mlab.com:31384/freecodecamp');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
 });
 
-/*GET API function. */
+/*GET new API function. */
 var url = '/new/:urlToShorten(*)';
 
 router.get(url, (req, res, next) => {
   var { urlToShorten } = req.params;
+
   // use regex for validating url
   var regex = /^((https?|ftp|smtp):\/\/)?(www.)?[a-z0-9]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/;
   if(regex.test(urlToShorten)) {
-    // generate a random 4 digit number and save it as short url
-    var short = Math.floor(Math.random()*100000).toString();
-    var data = new shortUrl({
-        originalUrl: urlToShorten,
-        shortUrl: short
-    });
-
-    data.save();
-    return res.json(data);
+     // main shorturl generator
+     generateShort();
   } else {
-    // show invalid url but don't save it to the database
-    var data = new shortUrl({
+    // show the invalid url but don't save it into the database
+    var data = {
       originalUrl: urlToShorten,
       shortUrl: 'InvalidURL'
-    });
+    };
+    return res.json(data);
+  }
 
+  // responsible to generating a valid shorturl
+  function generateShort() {
+    // generate a new random number and check if it exists in the database
+    var random = Math.floor(Math.random()*100000).toString();
+    isNewShort(random, () => {
+      // if it is a new shorturl, create the data
+      createData(random);
+    });
+  }
+
+  // this function creates the data, call update and display it
+  function createData(short) {
+    var data = {
+        originalUrl: urlToShorten,
+        shortUrl: short,
+        dateAdded: new Date()
+    };
+    updateToDatabase(data);
     return res.json(data);
   }
 });
 
-// TODO Query database
+// helper function used to check if the generated number is in the database
+function isNewShort(random, callback) {
+  mongo.connect(process.env.MONGODB_URI ||
+    'mongodb://fccuser:fCcUsER61@ds131384.mlab.com:31384/freecodecamp',
+    (err, db) => {
+     if (err) throw err;
+     var shorturls = db.collection('shorturls');
 
+     shorturls.find({shortUrl: random}, {_id: 1}).toArray((err, doc) => {
+       if (err) throw err;
+       // speedup hasOwnProperty call
+       var hasOwnProperty = Object.prototype.hasOwnProperty;
+
+       if (isEmpty(doc)) {
+         // this is a new number
+         callback();
+       } else {
+         // the number exists, restart the process and generate new shorturl
+         generateShort();
+       }
+       db.close();
+    });
+   });
+};
+
+// helper function, checks whether an object has valid property
+function isEmpty(obj) {
+  if (obj == null) return true;
+  if (obj.length > 0) return false;
+  if (obj.length === 0) return true;
+  if (typeof obj !== "object") return true;
+  for (var key in obj) {
+    if (hasOwnProperty.call(obj, key)) return false;
+  };
+  return true;
+};
+
+// update the new data to database
+function updateToDatabase(obj) {
+  mongo.connect(process.env.MONGODB_URI ||
+    'mongodb://fccuser:fCcUsER61@ds131384.mlab.com:31384/freecodecamp',
+    (err, db) => {
+     if (err) throw err;
+     var col = db.collection('shorturls');
+
+     col.insert(obj, (err, data) => {
+         if (err) throw err;
+         db.close();
+     });
+  });
+};
+
+// TODO GET shorturl API function
 
 module.exports = router;
